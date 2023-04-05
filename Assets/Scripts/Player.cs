f2f057a08043be5d03caa77ebb6c0f70638a6c7f
Olga <37110188+OlgaNet@users.noreply.github.com>
2023-04-05 14:23:18 +0100

    39. Cool Down System
    39. Cool Down System

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -10,6 +11,11 @@ public class Player : MonoBehaviour
     //optinal value assigned
     [SerializeField]
     private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _canFire = -1f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +28,15 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculateMovement();
+
+        //if i hit space key
+        //spawn gameobject
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            //Debug.Log("Space Key Pressed");
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+        }
     }
 
     void CalculateMovement()
